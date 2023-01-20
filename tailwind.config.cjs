ebaf62135d399077501aa71ada1cb00b8f94ed7f
Vlad Blindu <vlad.blindu@m-tag.io>
2023-01-20 10:45:34 +0200

    start design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
+const theme = require('./src/lib/theme.json')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js,svelte,ts}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{html,js,svelte,ts}'
+    ],
+    theme: {
+        extend: {
+            colors: theme.colors
+        }
+    },
+    plugins: [
+    ]
 }
